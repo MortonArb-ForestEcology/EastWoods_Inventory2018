1e4513e75f9f697176a7a4f1f987952f17c09e7a
Christy Rollinson <crollinson@gmail.com>
2018-09-07 18:13:18 -0500

    Updated exploratory tree analyses: merge 2007 & 2018

--- Exploratory Analyses.R
+++ Exploratory Analyses.R
@@ -35,7 +35,7 @@ tree.2018$Vigor <- as.factor(tree.2018$Vigor)
 tree.2018 <- tree.2018[!is.na(tree.2018$Date),]
 # tree.2018 <- data.frame(tree.2018[!is.na(tree.2018$Date),])
 tree.2018 <- data.frame(tree.2018)
-tree.2018$Status <- ifelse(tree.2018$Canopy=="S", "dead", "live")
+tree.2018$Status <- as.factor(ifelse(tree.2018$Canopy=="S", "dead", "live"))
 summary(tree.2018)
 
 unique(tree.2018$Spp.Code)[order(unique(tree.2018$Spp.Code))]
@@ -59,8 +59,10 @@ tree.2007 <- stack(tree.2007a[,c("DBH.live", "DBH.dead")])
 names(tree.2007) <- c("DBH", "Status")
 tree.2007$Status <- car::recode(tree.2007$Status, "'DBH.live'='live'; 'DBH.dead'='dead'")
 tree.2007[,c("PlotID", "Spp.Code", "Spp.Name", "Native")] <- tree.2007a[,c("PlotID", "Spp.Code", "Spp.Name", "Native")]
-tree.2007 <- tree.2007[!(is.na(tree.2007$DBH) & !tree.2007$Spp.Code %in% c("??", "NONE")),]
+tree.2007 <- tree.2007[!(is.na(tree.2007$DBH) & !tree.2007$Spp.Code %in% c("??", "NONE")) & !(tree.2007$Spp.Code %in% c("??", "NONE") & tree.2007$Status=="dead"),]
+tree.2007[tree.2007$Spp.Code %in% c("??", "NONE") & is.na(tree.2007$DBH), "Status"] <- NA
 tree.2007 <- droplevels(tree.2007)
+tree.2007$PlotID <- sub("-", "", tree.2007$PlotID)
 summary(tree.2007)
 summary(tree.2007[is.na(tree.2007$DBH),])
 
@@ -74,22 +76,61 @@ unique(tree.2007$Spp.Code)[!unique(tree.2007$Spp.Code) %in% unique(tree.2018$Spp
 
 tree.2007[tree.2007$Spp.Code=="??",]
 tree.2007$Spp.Code <- car::recode(tree.2007$Spp.Code, "'??'='ERROR'; 'NONE'='NO TREES'; 'UNIDENTIFIED SP'='UNIDENTIFIED'")
-tree.2018$Spp.Code <- ifelse(substr(tree.2018$Spp.Code, nchar(tree.2018$Spp.Code), nchar(tree.2018$Spp.Code))==".", substr(tree.2018$Spp.Code, nchar(tree.2018$Spp.Code), nchar(tree.2018$Spp.Code)-1), tree.2018$Spp.Code)
+tree.2018$Spp.Code <- ifelse(substr(tree.2018$Spp.Code, nchar(tree.2018$Spp.Code), nchar(tree.2018$Spp.Code))==".", substr(tree.2018$Spp.Code, 1, nchar(tree.2018$Spp.Code)-1), tree.2018$Spp.Code)
 
 tree.2007[tree.2007$Spp.Code=="PSEMEN",]
 
+unique(tree.2018$Spp.Name)[order(unique(tree.2018$Spp.Name))]
 unique(tree.2007$Spp.Name)[order(unique(tree.2007$Spp.Name))]
+
+tree.2007$Spp.Name <- sub("species", "spp", tree.2007$Spp.Name)
+tree.2018$Spp.Name <- sub("sp.", "spp", tree.2018$Spp.Name)
+
 tree.2007[tree.2007$Spp.Name=="rttf",]
 tree.2007[tree.2007$Spp.Name=="??",]
-tree.2007$Spp.Name <- car::recode(tree.2007$Spp.Name, "'rttf'='Acer species'; '??'='ERROR'")
+tree.2007$Spp.Name <- car::recode(tree.2007$Spp.Name, "'rttf'='Acer spp'; '??'='ERROR'; 'Hickory spp'='Carya spp'; 'Fraxinus - Collections'='Fraxinus spp'; 'Unidentified spp'='Unidentified'; 'NONE'='No trees'; 'Cladrastis lutea'='Cladrastris kentukea'")
+tree.2018$Spp.Name <- car::recode(tree.2018$Spp.Name, "")
 
-# Dropping unnecessary row
-tree.2007 <- tree.2007[tree.2007$Spp.Code==""]
+tree.2018$Spp.Name <- car::recode(tree.2018$Spp.Name, "'Quercus alb'='Quercus alba'; 'tilia americana'='Tilia americana'; 'Cladastris kentukea'='Cladrastris kentukea'")
+
+# We have quite a bit of difference in tree IDs from year to year
+unique(tree.2018$Spp.Name)[!unique(tree.2018$Spp.Name) %in% unique(tree.2007$Spp.Name)]
+unique(tree.2007$Spp.Name)[!unique(tree.2007$Spp.Name) %in% unique(tree.2018$Spp.Name)]
+
+tree.2007[tree.2007$Spp.Name=="Ribes spp",]
+
+# Note; not all plots were sampled in both years
+unique(tree.2007$PlotID)[!unique(tree.2007$PlotID) %in% unique(tree.2018$PlotID)]
+unique(tree.2018$PlotID)[!unique(tree.2018$PlotID) %in% unique(tree.2007$PlotID)]
+
+# Final Summarires
+tree.2007$PlotID <- as.factor(tree.2007$PlotID)
+tree.2007$Spp.Name <- as.factor(tree.2007$Spp.Name)
+tree.2018$Spp.Code <- as.factor(tree.2018$Spp.Code)
+tree.2018$Spp.Name <- as.factor(tree.2018$Spp.Name)
+tree.2018$Status <- as.factor(tree.2018$Status)
+tree.2007$Year <- 2007
+tree.2018$Year <- 2018
 summary(tree.2007)
+summary(tree.2018)
 # ----------------------------
 
 # ----------------------------
+# Putting data together & looking at changes
 # ----------------------------
+dat.tree.all <- rbind(tree.2007[,c("Year", "PlotID", "Spp.Name", "Spp.Code", "Status", "DBH")], tree.2018[,c("Year", "PlotID", "Spp.Name", "Spp.Code", "Status", "DBH")])
+dat.tree.all$Genus <- as.factor(unlist(lapply(stringr::str_split(dat.tree.all$Spp.Name, " "), function(x) x[1])))
+dat.tree.all$BA <- pi*(dat.tree.all$DBH/2)^2 # in cm2
+summary(dat.tree.all)
+
+dat.tree.plot <- aggregate(dat.tree.all$BA, by=dat.tree.all[,c("Year", "PlotID", "Genus", "Status")], FUN=sum)
+names(dat.tree.plot)[which(names(dat.tree.plot)=="x")] <- c("BA.tot")
+summary(dat.tree.plot)
+
+# Come up with an additional data frame looking at changes by genus between 2007 & 2008
+
+# Load in Spatial File so we can look at patterns & changes as a map
 # ----------------------------
+
 
 # -----------------------------------------------------------
